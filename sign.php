e6cb6bc421f1e00f7296b4563b2fca9dbac97124
AlinaJakiyeva <82430242+AlinaJakiyeva@users.noreply.github.com>
2021-04-23 18:33:25 +0500

    Add files via upload

---
+++ sign.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Menu</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/css/ionicons.min.css">
+		<link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="regcss.css">
+		 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+		 		<style>
+		 a {
+    color: white;
+    text-decoration: none;
+}
+
+a:hover 
+{
+     color:white; 
+     text-decoration:none; 
+     cursor:pointer;  
+}
+.back{
+     background-image:url(imgs/wallp.jpg);
+     height: 1080px;
+}
+</style>
+  </head>
+<body>
+
+<nav>
+	<a href="home.html">Home</a>
+	<a href="attract.html">Attractions</a>
+	<a href="hotel.html">Hotels</a>
+	<a href="contact.html">About Us</a>
+	<a href="sign.php">Sign In</a>
+	<a href="login.php">Login</a>
+	<a href="#">LOGO</a>
+	<div class="animation start-home"></div>
+</nav>
+
+
+
+<div class="page">
+  <div class="container">
+    <div class="left">
+      <div class="login">Registration</div>
+    </div>
+    <div class="right">
+      <div class="form" >
+      	        <label for="email">Email</label>
+        <input type="email" id="email">
+        <label for="password">Password</label>
+        <input type="password" id="password">
+                <label for="email">Email</label>
+        <input type="email" id="email">
+        <label for="password">Password</label>
+        <input type="password" id="password">
+        <label for="email">Email</label>
+        <input type="email" id="email">
+        <label for="password">Password</label>
+        <input type="password" id="password">
+        <input type="submit" id="submit" value="Submit">
+      </div>
+    </div>
+  </div>
+</div>
+
+  <script src="js/main.js"></script>
+</body>
+</html>
